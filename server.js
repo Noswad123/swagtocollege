a5cd15bf5461db96cdc10171c51278f00a932716
noswad123 <jamal.a.dawson@gmail.com>
2017-12-16 20:58:03 -0500

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const app =express();
-app.user(express.static(_dirname+'/public'));
+app.use(express.static(__dirname+'/public'));
 app.listen(process.env.PORT||8080);
 
 app.get('/*', function(req,res){
 
-res.sendFile(path.join(_dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 })
